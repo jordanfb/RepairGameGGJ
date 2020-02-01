50eb4e4d63faadde26093a8f482c9e18ceb43638
Jordan Faas-Bush <jmanfb@gmail.com>
2020-02-01 14:23:10 -0500

    Added yarnspinner helper to select and deselect options for controller support

---
+++ Assets/Scripts/DialogueSystem/YarnspinnerUIHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class YarnspinnerUIHelper : MonoBehaviour
+{
+    public Transform OptionButtonParent = null;
+    public void DeselectUI()
+    {
+        EventSystem.current.SetSelectedGameObject(null); // deselect!
+    }
+
+    public void SelectFirstOption()
+    {
+        for (int i = 0; i < OptionButtonParent.childCount; i++)
+        {
+            GameObject currObject = OptionButtonParent.GetChild(i).gameObject;
+            if (currObject.activeSelf && currObject.GetComponent<UnityEngine.UI.Button>())
+            {
+                EventSystem.current.SetSelectedGameObject(currObject);
+                break;
+            }
+        }
+    }
+}
