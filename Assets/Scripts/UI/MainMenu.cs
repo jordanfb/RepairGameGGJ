e5b75da042b67c3facc55e9d02e2801fd3a8b0a1
Jordan Faas-Bush <jmanfb@gmail.com>
2020-02-01 16:02:37 -0500

    Set mouse sensitivity to .2 by default the first load

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -10,21 +10,41 @@ public class MainMenu : MonoBehaviour
     public int gameplaySceneIndex = 2;
     public int uiSceneIndex = 3;
     public int levelSceneIndex = 4;
-
+    public bool initializeMouseSensitivity = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (initializeMouseSensitivity)
+        {
+            InitializeMouseSensitivity();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeMouseSensitivity()
     {
-        
+        // this is a really janky way to set the mouse sensitivity to .2f by default to balance out the controller input
+        if (!PlayerPrefs.HasKey("OneTimeRun.MouseSensitivity"))
+        {
+            Sinput.LoadControlScheme("MainControlScheme", true); // need to load it before we can save it
+
+            Sinput.mouseSensitivity = .2f; // initialize it once
+            SinputSystems.SinputFileIO.SaveControls(Sinput.controls, Sinput.controlSchemeName);
+            PlayerPrefs.SetString("OneTimeRun.MouseSensitivity", "done");
+        }
     }
 
+    [ContextMenu("Delete one time run keys")]
+    private void ClearOneTimePrefs()
+    {
+        if (PlayerPrefs.HasKey("OneTimeRun.MouseSensitivity"))
+        {
+            PlayerPrefs.DeleteKey("OneTimeRun.MouseSensitivity");
+        }
+    }
+
+    [ContextMenu("Play Game")]
     public void PlayGame()
     {
         SceneManager.LoadScene(gameplaySceneIndex);
@@ -32,6 +52,7 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(uiSceneIndex, LoadSceneMode.Additive);
     }
 
+    [ContextMenu("Quit Game")]
     public void Quit()
     {
 #if UNITY_EDITOR
@@ -41,6 +62,7 @@ public class MainMenu : MonoBehaviour
 #endif
     }
 
+    [ContextMenu("Control RemappingGame")]
     public void ControlRemapping()
     {
         // launch the SInput control remapping scene
